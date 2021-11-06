4faf7c07487cee2984381546e12adeb72e5fec52
dxogo <diogophc@ua.pt>
2021-11-06 23:01:54 +0000

    Lab02 Completed

---
+++ lab02/lab02_4/quotes/src/main/java/com/dxogo/quotes/Quote/AllShows.java
@@ -0,0 +1,22 @@
+package com.dxogo.Quote;
+
+public class AllShows {
+
+    private String name;
+    private int id;
+
+
+    public AllShows(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getId() { return this.id; }
+
+    public void setId(int id) { this.id = id; }
+}
